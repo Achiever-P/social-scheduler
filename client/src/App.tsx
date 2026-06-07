8bf680ea4ac841ffbd0ed447054acea61fcb10c0
Achiever-P <202351103@iiitvadodara.ac.in>
2026-06-07 11:11:16 +0530

    sidebar done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Scheduler from "./pages/Scheduler";
+import Accounts from "./pages/Accounts";
+import Dashboard from "./pages/Dashboard";
+import Layout from "./components/Layout";
+import AIComposer from "./pages/AIComposer";
 
 export default function App() {
     return (
@@ -8,6 +13,12 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
+                <Route element={<Layout />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/scheduler" element={<Scheduler />} />
+                    <Route path="/accounts" element={<Accounts />} />
+                    <Route path="/ai-composer" element={<AIComposer />} />
+                </Route>
             </Routes>
         </>
     );
